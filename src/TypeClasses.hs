db9f3feb00893833b4787596fb099e02aab9ac70
jtonic <antonel.pazargic@gmail.com>
2020-10-25 12:04:13 +0200

    new type class

--- src/TypeClasses.hs
+++ src/TypeClasses.hs
@@ -1,47 +1,11 @@
 module TypeClasses where
 
-elem' :: Eq a => a -> [a] -> Bool
-elem' _ [] = False
-elem' x (y : ys)
-    | x == y        = True
-    | otherwise     = elem' x ys
+class TooMany a where
+    tooMany :: a -> Bool
 
-data RGB = RGB Int Int Int
+instance TooMany Int where
+    tooMany a = a > 50
 
-instance Eq RGB where
-    (RGB r1 g1 b1) == (RGB r2 g2 b2) =
-        (r1 == r2) && (g1 == g2) && (b1 == b2)
-
-instance Show RGB where
-    -- show (RGB r g b) = show(r) ++ ":"
-    show (RGB r g b) = "RGB " ++ (show r) ++ " " ++ (show g)  ++ " " ++ (show b)
-
-red :: RGB
-red = RGB 255 0 0
-
-green :: RGB
-green = RGB 0 255 0
-
-blue :: RGB
-blue = RGB 0 0 255
-
-colors :: [RGB]
-colors = red : green : blue : []
-
-data RGB' = RGB' Int Int Int deriving (Eq, Show)
-
-elem'' :: RGB' -> [RGB'] -> Bool
-elem'' _ [] = False
-elem'' x (y : ys)
-    | x == y    = True
-    | otherwise = elem'' x ys
-
-red' =  RGB' 255 0 0
-green' = RGB' 0 255 0
-blue' = RGB' 0 0 255
-
-colors' = red' : green' : blue' : []
-
-hasRed = elem' red' colors'
-
-
+newtype Goats = Goats Int deriving (Eq, Show, Bounded)
+instance TooMany Goats where
+    tooMany (Goats n) = n > 50
