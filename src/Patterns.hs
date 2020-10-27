cadc47254cf9bc409fd31d83e5c021109a2832ac
jtonic <antonel.pazargic@gmail.com>
2020-10-27 06:58:39 +0200

    patterns in haskell

---
+++ src/Patterns.hs
@@ -0,0 +1,11 @@
+module Patterns where
+
+-- Builder pattern
+data Person = Person String Int Bool deriving Show
+
+builder :: String -> Int -> Bool -> Person
+builder = Person
+
+tony :: Person
+tony = builder "Antonel" 50 True
+
