afabe65fc5cd1184f3eda8cf8c8acdd758563e45
jtonic <antonel.pazargic@gmail.com>
2020-11-18 09:41:20 +0200

    Tests w/o quickcheck

---
+++ src/Math'.hs
@@ -0,0 +1,7 @@
+module Math' where
+
+isAllDigits :: String -> Bool
+isAllDigits val =
+  all
+    (== True)
+    $ map (\x -> x `elem` ['1', '2', '3', '4', '5', '6', '7', '8', '9']) val
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import Math'
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  putStrLn $ if isAllDigits "1234567" then "SUCCESS!" else "FAIL!!!"
+  putStrLn $ if isAllDigits "0000" then "SUCCESS!" else "FAIL!!!"
+  putStrLn $ if not $ isAllDigits "1st" then "SUCCESS!" else "FAIL!!!"
+  putStrLn $ if not $ isAllDigits "2nd" then "SUCCESS!" else "FAIL!!!"
+  putStrLn $ if not $ isAllDigits "aaaa" then "SUCCESS!" else "FAIL!!!"
+  return ()
