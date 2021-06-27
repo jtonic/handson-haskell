64a5eec04d8ed56246f6358c022bd0a210240dc2
Antonel-Ernest Pazargic <antonel-ernest.pazargic@ing.com>
2021-06-27 10:24:41 +0300

    Voting: First Pass the Post - part 2

--- src/Pih/Ch7/VotesOne.hs
+++ src/Pih/Ch7/VotesOne.hs
@@ -2,21 +2,20 @@
 
 module Pih.Ch7.VotesOne where
 
-import Data.List (sort)
 votes :: [String]
 votes = ["Liviu", "Irina", "Irina", "Tony", "Roxana", "Liviu", "Tony", "Liviu"]
 
 
-rduplic :: Ord a => [a] -> [a]
-rduplic [] = []
-rduplic (x: xs) = x : rduplic (filter (/= x) xs)
+rmduplic :: Ord a => [a] -> [a]
+rmduplic [] = []
+rmduplic (x: xs) = x : rmduplic (filter (/= x) xs)
 
 
 count :: Ord a => a -> [a] -> Int
 count a = length . filter (== a)
 
 results :: Ord a => [a] -> [(Int, a)]
-results as = [(count a as, a) | a <- rduplic as]
+results as = [(count a as, a) | a <- rmduplic as]
 
 winner :: Ord a => [a] -> a
 winner = snd . maximum . results
